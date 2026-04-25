1921ae8c95926c256219dc92720f7c48be392f82
purush2300 <reddypurushothm@gmail.com>
2026-04-25 21:32:29 +0530

    Initial commit

--- ROtate.java
+++ ROtate.java
@@ -1,31 +1,22 @@
 public class ROtate {
     public static void main(String[] args) {
-        int[]a={1,0,2,1,2,0,0,2,2};
-        int low=0;
-        int mid=0;
-        int high=a.length-1;
-        while (mid<=high) {
-            if(a[mid]==0){
-                int temp=a[mid];
-                a[mid]=a[low];
-                a[low]=temp;
-                low++;
-                mid++;
-            }
-            else if(a[mid]==1){
-                mid++;
-            }
-           else  if(a[mid]==2){
-                int temp=a[mid];
-                a[mid]=a[high];
-                a[high]=temp;
-                high--;
-            }
-        }
-
+        int[]a={7, 0, 0, 1, 7, 7, 2, 7, 7};
         for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i]+" ");
+            int count=0;
+            for (int j = i; j < a.length; j++) {
+                if(a[i]==a[j]){
+                    count++;
+                }
+            }
+            if(count>a.length/2){
+                System.out.println(a[i]);
+                return;
+            }
         }
+        
+        
+
+        System.out.println(-1);
       
         
     }
