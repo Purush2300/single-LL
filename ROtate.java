28604b659db447e084accaa3d698397da2106c98
purush2300 <reddypurushothm@gmail.com>
2026-04-26 11:16:39 +0530

    Inital commit

--- ROtate.java
+++ ROtate.java
@@ -1,38 +1,17 @@
 public class ROtate {
     public static void main(String[] args) {
-        int[][]a={{1,2,3},{4,5,6},{7,8,9}};
-        int r=a.length;
-        int c=a[0].length;
-        int []ans1=new int[r*c];
-        int k=0;
-       int top=0;
-       int bottom=r-1;
-       int left=0;
-       int right=c-1;
-       while (left<=right && top<=bottom) {
-        for (int i = left; i <= right; i++) {
-            ans1[k++]=a[top][i];
+        int[]a={10, 5, 2, 7, 1, 9};
+       int longest=0;
+       int k=15;
+       for (int i = 0; i < a.length; i++) {
+        int sum=0;
+        for (int j = i; j < a.length; j++) {
+            sum+=a[j];
+            if(sum==k){
+                longest=Math.max(longest, j-i+1);
+            }
+        }
        }
-       top++;
-       for (int i = top; i <=bottom; i++) {
-        ans1[k++]=a[i][right];
-       }
-       right--;
-       if(top<=bottom){
-                for (int i = right; i >=left; i--) {
-                ans1[k++]=a[bottom][i];
-                }
-                bottom--;
-       }
-     if(left<=right){
-                for (int i = bottom; i >=top; i--) {
-                ans1[k++]=a[i][left];
-                }
-                left++;
-     }
-       }
-       for (int i = 0; i < ans1.length; i++) {
-        System.out.print(ans1[i]+" ");
-       }
+       System.out.println(longest);
     }
 }
