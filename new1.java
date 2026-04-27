e8314d06a0e72fe4a5ae263980baac5b3a18a090
purush2300 <reddypurushothm@gmail.com>
2026-04-27 22:00:37 +0530

    Initial commit

---
+++ new1.java
@@ -0,0 +1,16 @@
+public class new1 {
+    public static void main(String[] args) {
+        String a="aab";
+        String p="";
+        for (int i = 0; i < a.length(); i++) {
+         
+
+    if (i == 0 || a.charAt(i) != a.charAt(i - 1)) {
+        p += a.charAt(i);
+    }
+
+           
+        }
+        System.out.println(p);
+    }
+}
