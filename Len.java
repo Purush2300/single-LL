201f64c247504f7d5d8ed0b725af2d46d312d6cf
purush2300 <reddypurushothm@gmail.com>
2026-05-01 22:37:21 +0530

    Initial commit

---
+++ Len.java
@@ -0,0 +1,26 @@
+public class Len {
+    public static void main(String[] args) {
+       String s="(()((()(())()))()())";
+       int depth=0;
+       StringBuilder sb= new StringBuilder();
+       for (char c : s.toCharArray()) {
+        if(c=='('){
+            if(depth>0){
+                sb.append(c);
+                
+            }
+            depth++;
+        }
+            else{
+                depth--;
+                if(depth>0){
+                    sb.append(c);
+                }
+            }
+        
+       } 
+       System.out.println(sb.toString());
+    }
+
+   
+}
