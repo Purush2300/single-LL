6b9bea33afc0b63bee9f91c375f78f421c3d24aa
purush2300 <reddypurushothm@gmail.com>
2026-05-04 22:52:05 +0530

    Initial commit

--- Problems/Problem2String.java
+++ Problems/Problem2String.java
@@ -4,34 +4,19 @@ public class Problem2String {
     public static void main(String[] args) {
         String name="ceghij";
         String novol="";
+        name.toLowerCase();
         for (int i = 0; i < name.length(); i++) {
-            char c=name.charAt(i);
-            if(isvowel(c,name)==true){
-                
-            }
-        }
-       
-        
-        System.out.println(novol);
-    }
-
-    private static boolean isvowel(char c,String name) {
-
-
-            for (int i = 0; i < name.length(); i++) {
-            if(c=='a'|| c=='e'||c=='i'
-            ||c=='o'||c=='u' || (c>=0 && c<=9))
+            if(name.charAt(i)=='a'|| name.charAt(i)=='e'||name.charAt(i)=='i'
+            ||name.charAt(i)=='o'||name.charAt(i)=='u' )
             {
-                return true;
+
             }
            
-           
+               else{ 
+                novol+=name.charAt(i);
+            }
             
         }
-
-
-
-        return false;
+        System.out.println(novol);
     }
-
 }
