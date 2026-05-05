9c6b7f3e73fd68b5116ee51d70bef58923c5b8a0
purush2300 <reddypurushothm@gmail.com>
2026-05-05 21:48:49 +0530

    Initial commit

--- Problems/Problemx.java
+++ Problems/Problemx.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Problemx {
     public static void main(String[] args) {
-        String s="eabdc";
+        String s="urmasernalsdwpoermWD.DFMWPOEROSD,skldfmwfjsmdf";
        char []arr=s.toCharArray();
        Arrays.sort(arr);
        System.out.println(new String(arr));
