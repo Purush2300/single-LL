2d95ce6072fad0ccb50176ff2de991600ed3a7f9
purush2300 <reddypurushothm@gmail.com>
2026-05-05 22:07:37 +0530

    Initial commit

--- Problems/Problemx.java
+++ Problems/Problemx.java
@@ -4,10 +4,23 @@ import java.util.Arrays;
 
 public class Problemx {
     public static void main(String[] args) {
-        String s="urmasernalsdwpoermWD.DFMWPOEROSD,skldfmwfjsmdf";
-       char []arr=s.toCharArray();
-       Arrays.sort(arr);
-       System.out.println(new String(arr));
+        String s="jpmztf";
+       int cnt=0;
+        for (int i = 0; i <s.length(); i++) {
+            char c=s.charAt(i);
+            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+
+            }
+            else{
+                cnt++;
+            }
+        }
+        if(cnt%2==0){
+            System.out.println("SHE");
+        }
+        else{
+            System.out.println("HE");
+        }
        
     }
 }
