ae55817297f6a8d3a8edacbe55df2b9a41c6f899
purush2300 <reddypurushothm@gmail.com>
2026-07-13 13:33:55 +0530

    Initial commit

--- july133.java
+++ july133.java
@@ -7,6 +7,11 @@ class Node{
         this.data=data;
         this.next=null;
     }
+    public Node(int data,Node next){
+        this.data=data;
+        this.next=null;
+    }
+
     
 }
 
@@ -17,13 +22,19 @@ public class july133 {
     public static void main(String[] args) {
         int[]arr={1,2,3,4,5};
         Node head=single(arr);
+    //     printMsg(head);
+    //    head= insertFirst(head,10);
+    //      printMsg(head);
+        // head=insertBetween(head,30,3);
+        //  printMsg(head);
+    //    head= insertLast(head,6);
+        // printMsg(head);
+        // head=deleteFirst(head);
+        //    printMsg(head);
+    //    head= deletemiddle(head,2);
+        
+       head= deleteLast(head);
         printMsg(head);
-        insertFirst(head);
-        insertBetween(head);
-        insertLast(head);
-        deleteFirst(head);
-        deletemiddle(head);
-        deleteLast(head);
     }
 
     private static Node single(int[] arr) {
@@ -49,7 +60,63 @@ public class july133 {
         
     }
 
-    private static void insertFirst(Node head) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    private static Node insertFirst(Node head, int i) {
+       Node newNode=new Node(i);
+       
+        newNode.next=head;
+        head=newNode;
+        return head;
     }
+
+    private static Node insertBetween(Node head, int value, int position) {
+       Node NewNode=new Node(value);
+      
+       Node temp=head;
+    
+        for(int i=1;i<position-1;i++){
+            temp=temp.next;
+        }
+       
+        NewNode.next=temp.next;
+        temp.next=NewNode;
+
+
+        return head;
+    }
+
+    private static Node insertLast(Node head, int i) {
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        Node newNode=new Node(i);
+        temp.next=newNode;
+        
+        return head;
+    }
+
+    private static Node deleteFirst(Node head) {
+       head=head.next;
+       return head;
+    }
+
+    private static Node deletemiddle(Node head, int pos) {
+       Node temp=head;
+       for(int i=0;i<pos-1;i++){
+        temp=temp.next;
+       }
+       temp.next=temp.next.next;
+       return head;
+    }
+
+    private static Node deleteLast(Node head) {
+       Node temp=head;
+       while(temp.next.next!=null){
+        temp=temp.next;
+       }
+       temp.next=null;
+       return head;
+    }
+
+    
 }
